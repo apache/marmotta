bfd302178616383c516489824a016cad25508605
Sergio Fernández <wikier@apache.org>
2013-02-23 11:30:45 +0100

    added proper license headers to the test source code too

--- client/marmotta-client-java/src/test/java/at/newmedialab/lmf/client/test/sparql/SPARQLIT.java
+++ client/marmotta-client-java/src/test/java/at/newmedialab/lmf/client/test/sparql/SPARQLIT.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2012 Salzburg Research
+/**
+ * Copyright (C) 2013 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package at.newmedialab.lmf.client.test.sparql;
 
 import at.newmedialab.lmf.client.ClientConfiguration;
