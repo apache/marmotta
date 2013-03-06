bccb29906b8f7397442e0e39867e721b386a6fd7
Sebastian Schaffert <sschaffert@apache.org>
2013-03-06 17:44:32 +0100

    started working on generic LDCache Sail

--- libraries/ldcache/ldcache-sail-generic/src/main/java/org/apache/marmotta/ldcache/generic/LinkedDataSail.java
+++ libraries/ldcache/ldcache-sail-generic/src/main/java/org/apache/marmotta/ldcache/generic/LinkedDataSail.java
@@ -1,9 +1,11 @@
 package org.apache.marmotta.ldcache.generic;
 
+import org.openrdf.sail.helpers.NotifyingSailWrapper;
+
 /**
  * Add file description here!
  * <p/>
  * Author: Sebastian Schaffert (sschaffert@apache.org)
  */
-public class LinkedDataSail {
+public class LinkedDataSail extends NotifyingSailWrapper {
 }
