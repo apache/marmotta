91a76bf1b87b4937db60262543f445ecf35fe440
Sebastian Schaffert <sschaffert@apache.org>
2013-08-05 16:26:57 +0200

    more sophisticated concurrency testing with resource reuse

--- libraries/kiwi/kiwi-triplestore/src/test/java/org/apache/marmotta/kiwi/test/ConcurrencyTestBase.java
+++ libraries/kiwi/kiwi-triplestore/src/test/java/org/apache/marmotta/kiwi/test/ConcurrencyTestBase.java
@@ -16,6 +16,8 @@ import org.openrdf.repository.RepositoryConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -33,12 +35,15 @@ public abstract class ConcurrencyTestBase {
 
     protected static Logger logger;
 
+    private List<URI> resources = new ArrayList<>();
+
+    private List<Value> objects = new ArrayList<>();
+
     @Rule
     public ConcurrentRule concurrently = new ConcurrentRule();
 
     @Rule
     public RepeatingRule repeatedly = new RepeatingRule();
-
 
     @Rule
     public TestWatcher watchman = new TestWatcher() {
@@ -63,26 +68,9 @@ public abstract class ConcurrencyTestBase {
         RepositoryConnection con = repository.getConnection();
         try {
             for(int i=0; i< rnd.nextInt(1000); i++) {
-                URI subject = repository.getValueFactory().createURI("http://localhost/"+ RandomStringUtils.randomAlphanumeric(8));
-                URI predicate = repository.getValueFactory().createURI("http://localhost/"+ RandomStringUtils.randomAlphanumeric(8));
-                Value object;
-                switch(rnd.nextInt(6)) {
-                    case 0: object = repository.getValueFactory().createURI("http://localhost/"+ RandomStringUtils.randomAlphanumeric(8));
-                        break;
-                    case 1: object = repository.getValueFactory().createBNode();
-                        break;
-                    case 2: object = repository.getValueFactory().createLiteral(RandomStringUtils.randomAscii(40));
-                        break;
-                    case 3: object = repository.getValueFactory().createLiteral(rnd.nextInt());
-                        break;
-                    case 4: object = repository.getValueFactory().createLiteral(rnd.nextDouble());
-                        break;
-                    case 5: object = repository.getValueFactory().createLiteral(rnd.nextBoolean());
-                        break;
-                    default: object = repository.getValueFactory().createURI("http://localhost/"+ RandomStringUtils.randomAlphanumeric(8));
-                        break;
-
-                }
+                URI subject = randomURI();
+                URI predicate = randomURI();
+                Value object = randomObject();
                 con.add(subject,predicate,object);
                 tripleCount++;
             }
@@ -93,5 +81,53 @@ public abstract class ConcurrencyTestBase {
 
 
         logger.info("triple count: {}", tripleCount);
+    }
+
+
+    /**
+     * Return a random URI, with a 10% chance of returning a URI that has already been used.
+     * @return
+     */
+    protected URI randomURI() {
+        if(resources.size() > 0 && rnd.nextInt(10) == 0) {
+            // return a resource that was already used
+            return resources.get(rnd.nextInt(resources.size()));
+        } else {
+            URI resource = repository.getValueFactory().createURI("http://localhost/"+ RandomStringUtils.randomAlphanumeric(8));
+            resources.add(resource);
+            return resource;
+        }
+    }
+
+    /**
+     * Return a random RDF value, either a reused object (10% chance) or of any other kind.
+     * @return
+     */
+    protected Value randomObject() {
+        if(objects.size() > 0 && rnd.nextInt(10) == 0) {
+            return objects.get(rnd.nextInt(objects.size()));
+        } else {
+            Value object;
+            switch(rnd.nextInt(6)) {
+                case 0: object = repository.getValueFactory().createURI("http://localhost/"+ RandomStringUtils.randomAlphanumeric(8));
+                    break;
+                case 1: object = repository.getValueFactory().createBNode();
+                    break;
+                case 2: object = repository.getValueFactory().createLiteral(RandomStringUtils.randomAscii(40));
+                    break;
+                case 3: object = repository.getValueFactory().createLiteral(rnd.nextInt());
+                    break;
+                case 4: object = repository.getValueFactory().createLiteral(rnd.nextDouble());
+                    break;
+                case 5: object = repository.getValueFactory().createLiteral(rnd.nextBoolean());
+                    break;
+                default: object = repository.getValueFactory().createURI("http://localhost/"+ RandomStringUtils.randomAlphanumeric(8));
+                    break;
+
+            }
+            objects.add(object);
+            return object;
+        }
+
     }
 }
