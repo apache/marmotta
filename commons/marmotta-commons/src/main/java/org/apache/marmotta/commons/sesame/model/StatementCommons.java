dc2f18a1ebb37a432c6cb143a361191638dcdfd6
Sebastian Schaffert <sschaffert@apache.org>
2013-09-03 15:41:29 +0200

    started with a marmotta-commons class for statement utility methods, for the moment different implementations of Guava Equivalence to more properly solve the different semantics of triple equivalence in Sesame (see https://openrdf.atlassian.net/browse/SES-1924)

---
+++ commons/marmotta-commons/src/main/java/org/apache/marmotta/commons/sesame/model/StatementCommons.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.marmotta.commons.sesame.model;
+
+import com.google.common.base.Equivalence;
+import com.google.common.base.Objects;
+import org.openrdf.model.Statement;
+
+/**
+ * Provide some utility functions for managing statements (e.g. different forms of equivalence)
+ *
+ * @author Sebastian Schaffert (sschaffert@apache.org)
+ */
+public class StatementCommons {
+
+    /**
+     * Return triple equivalence, taking only into account subject, predicate and object
+     *
+     * @return
+     */
+    public static Equivalence<Statement> tripleEquivalence() {
+        return new Equivalence<Statement>() {
+            @Override
+            protected boolean doEquivalent(Statement a, Statement b) {
+                if(a == b) return true;
+
+                if(!Objects.equal(a.getSubject(), b.getSubject())) return false;
+                if(!Objects.equal(a.getPredicate(), b.getPredicate())) return false;
+                if(!Objects.equal(a.getObject(), b.getObject())) return false;
+                return true;
+            }
+
+            @Override
+            protected int doHash(Statement statement) {
+                return Objects.hashCode(statement.getSubject(), statement.getPredicate(), statement.getObject());
+            }
+        };
+    }
+
+    /**
+     * Return quadruple equivalence, taking into account subject, predicate, object, and context.
+     *
+     * @return
+     */
+    public static Equivalence<Statement> quadrupleEquivalence() {
+        return new Equivalence<Statement>() {
+            @Override
+            protected boolean doEquivalent(Statement a, Statement b) {
+                if(a == b) return true;
+
+                if(!Objects.equal(a.getSubject(), b.getSubject())) return false;
+                if(!Objects.equal(a.getPredicate(), b.getPredicate())) return false;
+                if(!Objects.equal(a.getObject(), b.getObject())) return false;
+                if(!Objects.equal(a.getContext(), b.getContext())) return false;
+                return true;
+            }
+
+            @Override
+            protected int doHash(Statement statement) {
+                return Objects.hashCode(statement.getSubject(), statement.getPredicate(), statement.getObject(), statement.getContext());
+            }
+        };
+
+    }
+
+}
