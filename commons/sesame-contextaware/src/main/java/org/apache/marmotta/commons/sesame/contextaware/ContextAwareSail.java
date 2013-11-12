b253b86ff0eccac54df34d7402e171c84d570677
Jakob Frank <jakob@apache.org>
2013-11-12 17:35:09 +0100

    MARMOTTA-363: moved module kiwi-contextaware to commons/sesame-contextaware

--- commons/sesame-contextaware/src/main/java/org/apache/marmotta/commons/sesame/contextaware/ContextAwareSail.java
+++ commons/sesame-contextaware/src/main/java/org/apache/marmotta/commons/sesame/contextaware/ContextAwareSail.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.marmotta.kiwi.contextaware;
+package org.apache.marmotta.commons.sesame.contextaware;
 
 import org.openrdf.model.Resource;
 import org.openrdf.sail.Sail;
