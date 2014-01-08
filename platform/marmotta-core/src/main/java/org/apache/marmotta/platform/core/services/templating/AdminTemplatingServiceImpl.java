ab9df2426f0c808ea96942fc9ea5a9b582c851b2
Jakob Frank <jakob@apache.org>
2014-01-08 10:23:17 +0100

    fixed some warnings

--- platform/marmotta-core/src/main/java/org/apache/marmotta/platform/core/services/templating/AdminTemplatingServiceImpl.java
+++ platform/marmotta-core/src/main/java/org/apache/marmotta/platform/core/services/templating/AdminTemplatingServiceImpl.java
@@ -19,11 +19,7 @@ package org.apache.marmotta.platform.core.services.templating;
 
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
