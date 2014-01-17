b28dc3edf93e299ea6d6d826b1bb3f34b9daf67f
Sergio Fernández <wikier@apache.org>
2014-01-17 11:27:58 +0100

    MARMOTTA-423: added more tests

--- libraries/ldclient/ldclient-provider-freebase/src/test/java/org/apache/marmotta/ldclient/test/freebase/TestFreebaseProvider.java
+++ libraries/ldclient/ldclient-provider-freebase/src/test/java/org/apache/marmotta/ldclient/test/freebase/TestFreebaseProvider.java
@@ -31,9 +31,22 @@ import org.junit.Test;
  */
 public class TestFreebaseProvider extends ProviderTestBase {
 
+    private static final String ASF = "http://rdf.freebase.com/ns/m.0nzm";
+
     private static final String MARMOTTA = "http://rdf.freebase.com/ns/m.0wqhskn";
 
     private static final String SERGIO = "http://rdf.freebase.com/ns/m.07zqbwz";
+
+    /**
+     * Tests accessing ASF's page from Freebase.
+     *
+     * @throws Exception
+     *
+     */
+    @Test
+    public void testASF() throws Exception {
+        testResource(ASF, "m.0nzm.sparql");
+    }
 
     /**
      * Tests accessing Marmotta's page from Freebase.
