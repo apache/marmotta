034af23bc441aeb9ab45a5119d995f97cfb92106
Jakob Frank <jakob@apache.org>
2014-03-18 15:23:49 +0100

    fixing license headers in the file (to make RAT happy)

--- build/git-tag-revision.sh
+++ build/git-tag-revision.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
-# simple shell script to get the git commit hash of the commit with the tag 
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# simple shell script to get the git commit hash of the commit with the tag
 # given as argument; used when generating the vote email for Apache Marmotta
 git show-ref -d -s $1 | grep '{}' | cut -f 1 -d ' '
--- platform/backends/marmotta-backend-bigdata/src/main/java/org/apache/marmotta/platform/backend/bigdata/BigDataStoreProvider.java
+++ platform/backends/marmotta-backend-bigdata/src/main/java/org/apache/marmotta/platform/backend/bigdata/BigDataStoreProvider.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.marmotta.platform.backend.bigdata;
 
 import com.bigdata.Banner;
