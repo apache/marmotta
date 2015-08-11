d3c5e0b476f2774e95694dff5a927d2b44313949
cuent <cuent@192.168.1.3>
2015-08-11 01:04:38 -0500

    modified script to upgrade 4 to 5 db version.

--- libraries/kiwi/kiwi-triplestore/src/main/resources/org/apache/marmotta/kiwi/persistence/pgsql/upgrade_base_004_005.sql
+++ libraries/kiwi/kiwi-triplestore/src/main/resources/org/apache/marmotta/kiwi/persistence/pgsql/upgrade_base_004_005.sql
@@ -13,11 +13,30 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
---add geom nodetype
+
+--creating new type 'geom'
+-- 1. rename the enum type 
+alter type nodetype rename to nodetype_old;
+-- 2. create new type
 CREATE TYPE nodetype AS ENUM ('uri','bnode','string','int','double','date','boolean','geom');
+-- 3. rename column(s) which uses our enum type
+alter table nodes rename column ntype to ntype_old;
+-- 4. add new column of new type
+alter table nodes add ntype nodetype;
+-- 5. copy values to the new column
+update nodes set ntype = ntype_old::text::nodetype;
+-- 6. remove old column and type
+alter table nodes drop column ntype_old;
+
+-- 3. delete old enum type
+drop type nodetype_old;
+
+
+--ALTER TYPE nodetype ADD VALUE 'geom'  AFTER  'boolean';
+
 
 --necessary for use spatial queries
-CREATE EXTENSION postgis;
+CREATE EXTENSION IF NOT EXISTS POSTGIS;
 
 --adding geometry colum
 ALTER TABLE nodes ADD COLUMN gvalue geometry;
