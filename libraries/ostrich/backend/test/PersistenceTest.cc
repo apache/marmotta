622a30099d3ed004de3d1869fe42f867d5bf81b7
Sebastian Schaffert <sschaffert@apache.org>
2015-12-19 16:53:28 +0100

    more testing

--- libraries/ostrich/backend/test/PersistenceTest.cc
+++ libraries/ostrich/backend/test/PersistenceTest.cc
@@ -16,6 +16,8 @@
 
 using namespace boost::filesystem;
 
+using testing::Contains;
+
 namespace marmotta {
 namespace rdf {
 namespace proto {
@@ -42,7 +44,7 @@ class PersistenceTest : public ::testing::Test {
     }
 
     ~PersistenceTest() {
-        LOG(INFO) << "Destroying Test DB";
+        LOG(INFO) << "Destroying Test DB: " << testdir.string();
         delete db;
         remove_all(testdir);
     }
@@ -70,6 +72,55 @@ TEST_F(PersistenceTest, TestAddStatements) {
         EXPECT_FALSE(it->hasNext());
     }
 }
+
+
+TEST_F(PersistenceTest, TestGetStatements) {
+    std::vector<rdf::proto::Statement> stmts = {
+            rdf::Statement(rdf::URI("http://example.com/s1"), rdf::URI("http://example.com/p1"),
+                           rdf::URI("http://example.com/o1")).getMessage(),
+            rdf::Statement(rdf::URI("http://example.com/s2"), rdf::URI("http://example.com/p1"),
+                           rdf::URI("http://example.com/o1")).getMessage(),
+            rdf::Statement(rdf::URI("http://example.com/s1"), rdf::URI("http://example.com/p2"),
+                           rdf::URI("http://example.com/o2")).getMessage(),
+            rdf::Statement(rdf::URI("http://example.com/s2"), rdf::URI("http://example.com/p2"),
+                           rdf::URI("http://example.com/o2")).getMessage(),
+            rdf::Statement(rdf::URI("http://example.com/s1"), rdf::URI("http://example.com/p3"),
+                           rdf::URI("http://example.com/o3")).getMessage(),
+    };
+
+    util::CollectionIterator<rdf::proto::Statement> it(stmts);
+    db->AddStatements(it);
+
+    EXPECT_EQ(5, db->Size());
+
+    rdf::Statement pattern1;
+    pattern1.setSubject(rdf::URI("http://example.com/s1"));
+    auto it1 = db->GetStatements(pattern1.getMessage());
+    for (int i=0; i<3; i++) {
+        ASSERT_TRUE(it1->hasNext());
+        EXPECT_THAT(stmts, Contains(it1->next()));
+    }
+    EXPECT_FALSE(it1->hasNext());
+
+    rdf::Statement pattern2;
+    pattern2.setObject(rdf::URI("http://example.com/o1"));
+    auto it2 = db->GetStatements(pattern2.getMessage());
+    for (int i=0; i<2; i++) {
+        ASSERT_TRUE(it2->hasNext());
+        EXPECT_THAT(stmts, Contains(it2->next()));
+    }
+    EXPECT_FALSE(it2->hasNext());
+
+    rdf::Statement pattern3;
+    pattern2.setPredicate(rdf::URI("http://example.com/p1"));
+    auto it3 = db->GetStatements(pattern2.getMessage());
+    for (int i=0; i<2; i++) {
+        ASSERT_TRUE(it3->hasNext());
+        EXPECT_THAT(stmts, Contains(it3->next()));
+    }
+    EXPECT_FALSE(it3->hasNext());
+}
+
 
 }
 }
